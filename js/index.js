bfd62263cfea0338820faeea744283ee39d007c5
yogesh cl <clyogesh5@gmail.com>
2018-10-07 09:41:07 +0530

    home  page and  event page

--- js/index.js
+++ js/index.js
@@ -3,13 +3,14 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 380,
+      "value": 200,
       "density": {
         "enable": true,
-        "value_area": 1000
+        "value_area": 2000
       }
     },
     "color": {
+		
       "value": "#ffffff"
     },
     "shape": {
@@ -114,20 +115,3 @@ particlesJS("particles-js", {
 
 /* ---- stats.js config ---- */
 
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
